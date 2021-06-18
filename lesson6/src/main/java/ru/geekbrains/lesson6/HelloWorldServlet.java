4e87dc195effebbd997e72161d3d013b6a3f6d72
Mikhail-Meshcheriakov <sekesh@mail.ru>
2021-06-18 11:40:12 +0700

    homework

---
+++ lesson6/src/main/java/ru/geekbrains/lesson6/HelloWorldServlet.java
@@ -0,0 +1,27 @@
+package ru.geekbrains.lesson6;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init() throws ServletException {
+        message = "Hello World!";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+}
